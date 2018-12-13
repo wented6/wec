28d64859d0c8f771f62cf137f6ca1af7ba1d0c9e
wented6 <44713416+wented6@users.noreply.github.com>
2018-12-13 12:17:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,6 @@ const client = new Discord.Client();
 
 
 client.on('ready', () => {
-  console.log(Logged in as ${client.user.tag}!);
 });
 client.on('message', msg => {
   if (msg.content === 'welcome') {
